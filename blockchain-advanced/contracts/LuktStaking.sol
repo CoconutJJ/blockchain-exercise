855828d91ec06f31957dcc5d007d84a8aaa80a88
David Yue <davidyue5819@gmail.com>
2022-06-02 05:06:17 -0400

    check division by 0

--- blockchain-advanced/contracts/LuktStaking.sol
+++ blockchain-advanced/contracts/LuktStaking.sol
@@ -1,27 +1,34 @@
 pragma solidity ^0.8.0;
 
-
 interface IERC20 {
-    function totalSupply() external view returns (uint);
+    function totalSupply() external view returns (uint256);
 
-    function balanceOf(address account) external view returns (uint);
+    function balanceOf(address account) external view returns (uint256);
 
-    function transfer(address recipient, uint amount) external returns (bool);
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
 
-    function allowance(address owner, address spender) external view returns (uint);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
 
-    function approve(address spender, uint amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
 
     function transferFrom(
         address sender,
         address recipient,
-        uint amount
+        uint256 amount
     ) external returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
-
 
 contract LuktStaking {
     struct StakeHolder {
@@ -33,68 +40,63 @@ contract LuktStaking {
 
     uint256 REWARD_PER_BLOCK = 1;
     uint256 DECIMALS = 1e18;
-    
+
     uint256 _lastBlock = 0;
     uint256 _totalSupply = 0;
     uint256 _accumulatedRewardsPerShare = 0;
-    
-    mapping(address => StakeHolder) _stakers;
 
+    mapping(address => StakeHolder) _stakers;
 
     constructor(address _stakingTokenAddress, uint256 _rewards_per_block) {
         _token = IERC20(_stakingTokenAddress);
         REWARD_PER_BLOCK = _rewards_per_block;
     }
 
-
     function updateRewards() private {
 
-        if (_totalSupply == 0) {
-            _lastBlock = block.number;
+        if (_totalSupply != 0) {
+            _accumulatedRewardsPerShare =
+                ((block.number - _lastBlock) * REWARD_PER_BLOCK * DECIMALS) /
+                _totalSupply;
         }
 
-        _accumulatedRewardsPerShare = (block.number - _lastBlock) * REWARD_PER_BLOCK * DECIMALS / _totalSupply;
         _lastBlock = block.number;
     }
 
     function harvest() public {
-
         updateRewards();
 
         StakeHolder storage s = _stakers[msg.sender];
 
-        uint256 rewards = s.amount * _accumulatedRewardsPerShare / DECIMALS - s.rewardDebt;
+        uint256 rewards = (s.amount * _accumulatedRewardsPerShare) /
+            DECIMALS -
+            s.rewardDebt;
 
-        s.rewardDebt = s.amount * _accumulatedRewardsPerShare / DECIMALS;
+        s.rewardDebt = (s.amount * _accumulatedRewardsPerShare) / DECIMALS;
 
         _token.transferFrom(address(this), msg.sender, rewards);
-        
     }
 
     function stake(uint256 amount) public {
-
         harvest();
-        
+
         StakeHolder storage s = _stakers[msg.sender];
 
         s.amount += amount;
-        
-        _totalSupply += amount;
 
+        _totalSupply += amount;
     }
 
     function withdraw() public {
-
         harvest();
 
         StakeHolder storage s = _stakers[msg.sender];
 
         _token.transferFrom(address(this), msg.sender, s.amount);
 
-        s.rewardDebt = s.amount * _accumulatedRewardsPerShare / DECIMALS;
+        s.rewardDebt = (s.amount * _accumulatedRewardsPerShare) / DECIMALS;
         s.amount = 0;
 
         _totalSupply -= s.amount;
     }
-
 }
